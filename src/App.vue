4d3d97867b02548a5a80d302e093a6db4f844560
jacobrees <jacob.rees97@yahoo.com>
2026-02-22 04:33:06 +0000

    Create navbar for project, import Inter google font

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
+<script setup>
+import gitHubLogo from "@/assets/github.svg";
+</script>
+
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div
+    class="w-full h-[100px] flex items-center justify-center bg-gray-800 border-b-2 border-gray-700"
+  >
+    <div class="max-w-[1100px] w-full m-16 flex items-center justify-between">
+      <div></div>
+      <h2 class="text-gray-200 text-4xl font-semibold">
+        Guitar Fretboard Generator
+      </h2>
+      <a href="#"
+        ><img class="w-[54px]" :src="gitHubLogo" alt="GitHub Logo"
+      /></a>
+    </div>
+  </div>
 </template>
