b8d9a5e57cff70001b0774fc194b9c858c3de225
jacobrees <jacob.rees97@yahoo.com>
2026-02-22 05:28:35 +0000

    Move navbar to its own Navigation.vue component and import into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
 <script setup>
-import gitHubLogo from "@/assets/github.svg";
+import Navigation from "@/components/Navigation.vue";
 </script>
 
 <template>
-  <div
-    class="w-full h-[100px] flex items-center justify-center bg-gray-800 border-b-2 border-gray-700"
-  >
-    <div class="max-w-[1100px] w-full m-16 flex items-center justify-between">
-      <div></div>
-      <h2 class="text-gray-200 text-4xl font-semibold">
-        Guitar Fretboard Generator
-      </h2>
-      <a href="#"
-        ><img class="w-[54px]" :src="gitHubLogo" alt="GitHub Logo"
-      /></a>
-    </div>
-  </div>
+  <Navigation />
 </template>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,18 @@
+<script setup>
+import gitHubLogo from "@/assets/github.svg";
+</script>
+
+<template>
+  <div
+    class="w-full h-[100px] flex items-center justify-center bg-gray-950 border-b-1 border-gray-500"
+  >
+    <div class="max-w-[1100px] w-full m-16 flex items-center justify-between">
+      <h2 class="text-gray-100 text-4xl font-semibold">
+        Guitar Fretboard Generator
+      </h2>
+      <a href="#"
+        ><img class="w-[64px]" :src="gitHubLogo" alt="GitHub Logo"
+      /></a>
+    </div>
+  </div>
+</template>
