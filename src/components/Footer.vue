4215e374794ae1dd1c701a21f374b03b1749bc1f
jacobrees <jacob.rees97@yahoo.com>
2026-02-22 15:20:06 +0000

    Correct typo in footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,6 @@
   <div
     class="w-full h-[46px] mt-4 border-t-2 border-gray-400 bg-gray-900 flex justify-center items-center"
   >
-    <p class="text-gray-50">Made With &lt;3 by Jacob</p>
+    <p class="text-gray-50">Made with &lt;3 by Jacob</p>
   </div>
 </template>
